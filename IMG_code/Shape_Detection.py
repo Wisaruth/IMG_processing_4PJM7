30dc77a32a450c8786b8001d6a34a757b4a6b014
Wisaruth <wisaruth73@hotmail.com>
2020-10-28 16:45:30 +0700

    add the new way to detect the symboles

--- IMG_code/Shape_Detection.py
+++ IMG_code/Shape_Detection.py
@@ -1,6 +1,6 @@
 import cv2
+import math
 import numpy as np
-
 from matplotlib import pyplot as plt
 
 def nothing(x):
@@ -11,52 +11,50 @@ max_lowThreshold = 100
 contours = []
 sign_imgs = []
 sign_name = [0,2,8]
-sign_name2 = ["Star","2Triangle","2Triangle"]
+sign_name2 = ["Star","Triangle","2Triangles"]
 
 
 window_name = "Edge Detection"
 bar_name = 'Thrshold'
-path = "C:/Users/ASUS/OneDrive/My work/Project_module7/IMG_test/"
+path = "C:/Users/wisar/OneDrive/My work/Project_module7/IMG_test/"
 map_img = cv2.imread(path+"Map_A.jpg")
 for j in sign_name :
     sign_img = cv2.imread(path+"img_test"+str(j)+".jpg",0)
     sign_imgs.append(sign_img)
 
+def unsharp_image (img_):
+    blur_img = cv2.GaussianBlur(img_,(5,5),0)
+    unsharp_image = cv2.addWeighted(img_, 2, blur_img, -1, 0, img_)
+    return  unsharp_image
 
-def CannyThreshold(low_thres_cannay,img_,approx_mode,approx_thres,area_thres): 
-    # cv2.CHAIN_APPROX_SIMPLE cv2.CHAIN_APPROX_NONE
-    # approx_thres = 0.08
+def find_canny(low_thres_cannay,img_):
     kernel_size_cannay = 3
     ratio_ = 2
-    pass_cnts = []
-    pass_boxcoord =[]
-    pass_approx = []
-    gray_img = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-    gray_img = cv2.medianBlur(gray_img,5)
-    blur_img = cv2.GaussianBlur(gray_img,(5,5),0)
-    unsharp_image = cv2.addWeighted(gray_img, 2, blur_img, -1, 0, gray_img)
-    detected_edges = cv2.Canny(unsharp_image, low_thres_cannay, low_thres_cannay*ratio_,kernel_size_cannay)
-    if approx_mode is True:
-        mode_ = cv2.CHAIN_APPROX_SIMPLE
-    else :
-        mode_ = cv2.CHAIN_APPROX_NONE
-    _,cnts_,_= cv2.findContours(detected_edges,cv2.RETR_TREE,mode_)
-    for cnt in cnts_:
-        area = cv2.contourArea(cnt)
-        if area > area_thres:
-            pass_cnts.append(cnt)
-            if approx_mode :
-                rect = cv2.boundingRect(cnt)
-                pass_boxcoord.append(rect)
-                epsilon = approx_thres * cv2.arcLength(cnt, True)
-                approx = cv2.approxPolyDP(cnt, epsilon, True)
-                pass_approx.append(approx)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    blur_img = cv2.medianBlur(img_,5)
+    blur_img = cv2.GaussianBlur(blur_img,(5,5),0)
+    edges = cv2.Canny(blur_img, low_thres_cannay, low_thres_cannay*ratio_,kernel_size_cannay)
+    return edges
 
-    return detected_edges,pass_cnts,pass_boxcoord,pass_approx
+def find_contours(edges_,mode_,area_thres):
+    approx_thres = 0.08
+    pass_data ={"cnts":[],"boxcoords":[],"approxs":[]}
+    _,cnts_,_= cv2.findContours(edges_,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt_ in cnts_:
+        area = cv2.contourArea(cnt_)
+        
+        if area > area_thres  :
+            
+            pass_data["cnts"].append(cnt_)
+            rect = cv2.boundingRect(cnt_)
+            pass_data["boxcoords"].append(rect)
+            epsilon = approx_thres * cv2.arcLength(cnt_, True)
+            approx_ = cv2.approxPolyDP(cnt_, epsilon, True)
+            pass_data["approxs"].append(approx_)
+    
+    return pass_data
 
 def check_matching(img_,temp_,match_thres,cnt_,boxcoord_):
-    
-    #count=0
     x,y,w,h = boxcoord_
     crop_img = img_[y:y+h,x:x+w]
     crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
@@ -69,24 +67,16 @@ def check_matching(img_,temp_,match_thres,cnt_,boxcoord_):
     else :
         return False
 
-    #cv2.rectangle(img_ , (x, y), (x+w, y+h), (0, 255, 0), 2)
     
-cap = cv2.VideoCapture(0)
+    
+#cap = cv2.VideoCapture(0)
 cv2.namedWindow(window_name)    
 font = cv2.FONT_HERSHEY_SIMPLEX 
-  
-
-
 fontScale = 1
 thickness = 2
-   
-# Using cv2.putText() method 
-  
-
-#edge_img = CannyThreshold(56,img)
-edge_img,contours,coordinate,approx = CannyThreshold(65,map_img,True,0.08,100)
+edge_mask = find_canny(22,map_img)
+"""
 last_coord = [0,0,0]
-
 for j in range(len(sign_name)):
     for i in range(len(contours)):
         check= check_matching(map_img,sign_imgs[j],0.6,contours[i],coordinate[i])
@@ -101,15 +91,36 @@ for j in range(len(sign_name)):
                 print(sign_name[j]) 
                 last_coord[0],last_coord[1] = mid_[0],mid_[1]
                 last_coord[2] = max([w,h])
-        
-            
-cv2.imshow(window_name, map_img)
+"""        
+
+linesP=cv2.HoughLinesP(edge_mask,1,np.pi/180, 100, 500, 40)
+
+last_line =[0,0,0,0]
+dis_chess =[0,0]
+if linesP is not None:
+    for i in range(0, len(linesP)):
+        l = linesP[i][0]
+        cv2.line(edge_mask, (l[0], l[1]), (l[2], l[3]), 0,1)
+        dis_chess[0],dis_chess[1] = max(abs(last_line[0]-l[0]),abs(last_line[1]-l[1])),max(abs(last_line[2]-l[2]),abs(last_line[3]-l[3]))
+        #if dis_chess[0]>10 and dis_chess[1] > 10:
+        #    cv2.line(map_img, (l[0], l[1]), (l[2], l[3]), (0),2)
+        last_line=l
+
+dataset =find_contours(edge_mask,True,50)
+
+for index in range(len(dataset["cnts"])) :
+    print(len(dataset["approxs"][index]))
+    x,y,w,h = dataset["boxcoords"][index]
+    cv2.rectangle(map_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow(window_name, map_img)
+    cv2.waitKey()
+"""
 while True:
     key = cv2.waitKey(10)
     if key == ord('q') or key == ord('Q') :
         cv2.destroyWindow(window_name)
         break       
-
+"""
 #cv2.createTrackbar(bar_name,window_name,0,max_lowThreshold,nothing)
 """
 while True:
@@ -122,21 +133,4 @@ while True:
     if key == ord('q') or key == ord('Q') :
         cv2.destroyWindow(window_name)
         break
-"""
-"""
-for cnt in contours:
-    approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-    cv2.drawContours(draw_img, [approx], 0, (0), 2)
-    cv2.imshow(window_name,draw_img)
-    if len(approx) == 3:
-        print("Triangle")
-    elif len(approx) == 4:
-        print("Rectangle")
-    elif len(approx) == 5:
-        print("Pentagon")
-    elif 6 < len(approx) < 15:
-        print("Ellipse")
-    else:
-        print("Circle")
-    cv2.waitKey()
 """
