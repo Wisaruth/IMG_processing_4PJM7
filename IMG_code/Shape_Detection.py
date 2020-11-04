b044f00471bd5b0abca7eb1f1172aebe2020ff7c
Wisaruth <wisaruth73@hotmail.com>
2020-11-04 14:01:30 +0700

    just push

--- IMG_code/Shape_Detection.py
+++ IMG_code/Shape_Detection.py
@@ -1,47 +1,23 @@
 import cv2
 import math
 import numpy as np
-<<<<<<< HEAD
-=======
-import math
-
->>>>>>> 8414e4bc842545a4259334518a52fd08f44b468b
 from matplotlib import pyplot as plt
 
 def nothing(x):
     pass
 
 
-<<<<<<< HEAD
 class Symbol:
     def __init__(self, name,mid,box):
         self.name = name
         self.mid = mid
         self.box = box
-=======
-max_lowThreshold = 100
-contours = []
-sign_imgs = []
-sign_name = [0,2,8]
-sign_name2 = ["Star","Triangle","2Triangles"]
-
-
-window_name = "Edge Detection"
-bar_name = 'Thrshold'
-path = "C:/Users/ASUS/OneDrive/My work/Project_module7/IMG_test/"
-map_img = cv2.imread(path+"Map_A.jpg")
-for j in sign_name :
-    sign_img = cv2.imread(path+"img_test"+str(j)+".jpg",0)
-    _,sign_img = cv2.threshold(sign_img,127,255,cv2.THRESH_BINARY)
-    sign_imgs.append(sign_img)
->>>>>>> 8414e4bc842545a4259334518a52fd08f44b468b
 
 def unsharp_image (img_):
     blur_img = cv2.GaussianBlur(img_,(5,5),0)
     result = cv2.addWeighted(img_, 2, blur_img, -1, 0, img_)
     return result
 
-<<<<<<< HEAD
 def find_canny(low_thres_cannay,gray_img):
     kernel_size_cannay = 3
     ratio_ = 2
@@ -92,40 +68,6 @@ def hough_transform (edges_,minpoint,maxgap):# 500, 20
 
 
 def check_matching(img_,temp_,match_thres,cnt_,boxcoord_):
-=======
-def cannythreshold(low_thres_cannay,img_): 
-    kernel_size_cannay = 3
-    ratio_ = 3
-    gray_img = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-    gray_img = cv2.medianBlur(gray_img,5)
-    blur_img = cv2.GaussianBlur(gray_img,(5,5),0)
-    edges = cv2.Canny(blur_img, low_thres_cannay, low_thres_cannay*ratio_,kernel_size_cannay)
-    return edges
-
-def findcountour(edges,approx_thres,area_thres,mode_):
-    # cv2.CHAIN_APPROX_SIMPLE cv2.CHAIN_APPROX_NONE
-    # approx_thres = 0.08
-    pass_cnts = []
-    pass_boxcoord =[]
-    pass_approx = []
-    _,cnts_,_= cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-    if mode_ :
-        for cnt in cnts_:
-            area = cv2.contourArea(cnt)
-            if area > area_thres:
-                pass_cnts.append(cnt)
-                rect = cv2.boundingRect(cnt)
-                pass_boxcoord.append(rect)
-                epsilon = approx_thres * cv2.arcLength(cnt, True)
-                approx = cv2.approxPolyDP(cnt, epsilon, True)
-                pass_approx.append(approx)
-        return pass_cnts,pass_boxcoord,pass_approx
-    else:
-        return pass_cnts
-
-def check_matching(img_,temp_,match_thres,cnt_,boxcoord_):
-    #count=0
->>>>>>> 8414e4bc842545a4259334518a52fd08f44b468b
     x,y,w,h = boxcoord_
     crop_img = img_[y:y+h,x:x+w]
     crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
@@ -139,7 +81,6 @@ def check_matching(img_,temp_,match_thres,cnt_,boxcoord_):
     else :
         return False
 
-<<<<<<< HEAD
 def find_symWithCorner(cntset_,target_syms,img_,mode_show):
     pass_sym =[]
     last_coord =[0,0,0]
@@ -178,18 +119,6 @@ for index in range(len(cntset["cnts"])):
     cv2.imshow(window_name,map_img)
     cv2.waitKey()
  """
-=======
-    
-cv2.namedWindow(window_name)
-
-"""
-font = cv2.FONT_HERSHEY_SIMPLEX 
-fontScale = 1
-thickness = 2
-edge_img = cannythreshold(65,map_img)
-contours,coordinate,approx =findcountour(edge_img,0.08,100,True)
-last_coord = [0,0,0]
->>>>>>> 8414e4bc842545a4259334518a52fd08f44b468b
 
 """
 sign_imgs = []
@@ -213,7 +142,6 @@ for j in range(len(sign_name)):
                 print(sign_name[j]) 
                 last_coord[0],last_coord[1] = mid_[0],mid_[1]
                 last_coord[2] = max([w,h])
-<<<<<<< HEAD
 """        
 
 """
@@ -235,27 +163,6 @@ cv2.waitKey()
 """
 cv2.createTrackbar(bar_name,window_name,0,max_lowThreshold,nothing)
 bar_name = 'Thrshold'
-=======
-"""
-
-img = cannythreshold(22,map_img)
-lines = cv2.HoughLinesP(img, 1, np.pi / 180, 50, None, 60, 20)
-if lines is not None:
-        for i in range(0, len(lines)):
-            l = lines[i][0]
-            cv2.line(map_img, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)      
-            
-cv2.imshow(window_name, map_img)
-while True:
-    key = cv2.waitKey(10)
-    if key == ord('q') or key == ord('Q') :
-        cv2.destroyWindow(window_name)
-        break  
-    
-"""
-cv2.createTrackbar(bar_name,window_name,0,max_lowThreshold,nothing)
-
->>>>>>> 8414e4bc842545a4259334518a52fd08f44b468b
 while True:
     key = cv2.waitKey(10)
     #ret,frame = cap.read()
@@ -270,7 +177,3 @@ while True:
         cv2.destroyWindow(window_name)
         break
 """
-<<<<<<< HEAD
-=======
-
->>>>>>> 8414e4bc842545a4259334518a52fd08f44b468b
